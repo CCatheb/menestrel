7016e7cc55449d517863369e50cfba0493c396e2
Cédric Cathebras <cathebras-cedric@orange.fr>
2024-11-02 22:17:27 +0100

    Bot can now play music

--- src/BTDC/bot.py
+++ src/BTDC/bot.py
@@ -9,11 +9,15 @@ class MenestrelBot(discord.Client):
     Define all methods needed to be executed by the bot on API calls below.
     """
 
+    # TODO: All theses methods should be in a Cog
+    # TODO: Add a leave command
+    # TODO: Cannot switch music, should add a stop music method
     def __init__(self, config, intents, **options):
         super().__init__(intents=intents, **options)
 
         self.logger = CustomLogger.get_logger("BTDC")
         self.mf = MusicFinder(config)
+        self.config = config
 
 
     async def on_ready(self):
@@ -42,7 +46,15 @@ class MenestrelBot(discord.Client):
         result = self.mf.get_music_by_scene(scene)
         await channel.send(result)
 
+        voice_client = discord.utils.get(self.voice_clients)
+        audio_source = discord.FFmpegPCMAudio(result, executable=self.config["Discord"]["ffmpeg"])
+        voice_client.play(audio_source, after=None)
+
     async def change_phase(self, phase: str):
         channel = channel = self.get_channel(1219376772236312608)
         result = self.mf.get_music_by_phase(phase)
         await channel.send(result)
+
+        voice_client = discord.utils.get(self.voice_clients)
+        audio_source = discord.FFmpegPCMAudio(result, executable=self.config["Discord"]["ffmpeg"])
+        voice_client.play(audio_source, after=None)
